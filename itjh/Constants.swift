6a93a419b43a7acf43e124e344892eb382476693
itjhDev <github@itjh.com.cn>
2015-01-26 20:53:31 +0800

    添加注释
    添加注释

--- itjh/Constants.swift
+++ itjh/Constants.swift
@@ -13,9 +13,10 @@ import Foundation
 // 获取文章列表
 let GET_ARTICLE = "http://m.itjh.com.cn:8080/mitjh/ArticleServer/queryArticleListByNew/"
 
-
+// 根据文章ID获取电影详情
 var GET_ARTICLE_ID = "http://m.itjh.com.cn:8080/mitjh/ArticleServer/queryArticleById/"
 
+// 根据分类获取文章列表
 var GET_ARTICLE_Category = "http://m.itjh.com.cn:8080/mitjh/ArticleServer/queryArticleListByCategory"
 
 
